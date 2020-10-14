549c89866a2a1d0e2e79ceb3aa797e39780c059d
AungTayZarMaung25 <aungtayzarmaung.mhs@gmail.com>
2020-10-14 20:38:07 +0630

    Updates

---
+++ precache-manifest.2ce5f9a8a7348e7330ecaa040bc15dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0ef1dda44d93eaac3593ebdc951d3d",
+    "url": "/Voice-recognition/index.html"
+  },
+  {
+    "revision": "7538cef612350ad185c4",
+    "url": "/Voice-recognition/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "22f7a35e9df17a589b71",
+    "url": "/Voice-recognition/static/js/2.cf974a1e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Voice-recognition/static/js/2.cf974a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7538cef612350ad185c4",
+    "url": "/Voice-recognition/static/js/main.b86d744a.chunk.js"
+  },
+  {
+    "revision": "6772509372024116fc38",
+    "url": "/Voice-recognition/static/js/runtime-main.131caf7f.js"
+  }
+]);
